04b561c2a4e2aa93a191b5d5bef34222072e96bd
Yaz0612 <yazmin.acosta.jimenez@gmail.com>
2025-01-23 17:19:19 -0600

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'TotillaOs',
-  webDir: 'dist'
-};
-
-export default config;
